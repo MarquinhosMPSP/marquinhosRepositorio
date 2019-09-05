6f57eea3a0489ccafd72b2298ef3265982bbe7a3
Daniel <marquinhosmpsp@outlook.com>
2019-09-05 04:34:43 -0300

    adicionando autenticação de usuário

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 const usuarioController = require('./controller/usuarioController')
 
 routes.get('/usuario', usuarioController.index);
+routes.post('/autenticar', usuarioController.autenticar);
 routes.get('/usuario/:id', usuarioController.pesquisar)
 routes.post('/usuario', usuarioController.cadastro)
 routes.put('/usuario/:id', usuarioController.atualizar)
