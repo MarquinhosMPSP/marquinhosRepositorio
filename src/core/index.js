15a87ef306ffe4e2bfc782dbddfe6d4c6b5c5718
Daniel <marquinhosmpsp@outlook.com>
2019-09-26 17:40:35 -0300

    atualizando puppeteer

--- src/core/index.js
+++ src/core/index.js
@@ -4,9 +4,7 @@ const Scraper = require('./model/scraper');
 const run = async() => {
     let mainUrl = "http://ec2-18-231-116-58.sa-east-1.compute.amazonaws.com"
     
-    const scraper = new Scraper({
-        headless: false
-    });
+    const scraper = new Scraper({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 
     let isLogin = false
 
