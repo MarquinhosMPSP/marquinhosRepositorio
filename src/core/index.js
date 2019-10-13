2db73899c15fd4c0f98896aaca0ca9063ec2a6c7
Daniel <marquinhosmpsp@outlook.com>
2019-10-13 18:22:16 -0300

    Subindo todos os portais

--- src/core/index.js
+++ src/core/index.js
@@ -41,15 +41,15 @@ module.exports = {
       return await scraper.doRun(async (browser, page) => {
         await page.goto(mainUrl);
         const portais = Promise.all([
-          // jucesp(browser),
-          // siel(browser),
-          // sivec(browser),
-          // cadesp(browser),
-          // censec(browser),
-          // infocrim(browser),
-          // arpenp(browser),
-          // caged(browser),
-          // detran(browser)
+          jucesp(browser),
+          siel(browser),
+          sivec(browser),
+          cadesp(browser),
+          censec(browser),
+          infocrim(browser),
+          arpenp(browser),
+          caged(browser),
+          detran(browser),
           arisp(browser)
         ]).then(async data => {
           await browser.close();
